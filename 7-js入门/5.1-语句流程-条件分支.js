54b35efec366e7d28ce7afcd09a21bcc48637ca9
qiankun <qk83764579@sina.com>
2017-02-06 22:21:38 +0800

    添加电话，头部背景图修改

--- 7-js入门/5.1-语句流程-条件分支.js
+++ 7-js入门/5.1-语句流程-条件分支.js
@@ -4,16 +4,16 @@
  * 不是boolean类型则执行隐式转换，转换规则参考逻辑非(!)
  */
 var a;
-a=1;
-if (a===1){
+a = 1;
+if (a === 1) {
     console.log('if条件命中');
 }
 /**
  * switch
  */
 var b;
-b=0;
-switch (b){
+b = 0;
+switch (b) {
     case 0:
         console.log('b等于0');
         break;
@@ -21,6 +21,6 @@ switch (b){
         console.log('b等于1');
         break;
     /*case null&&0:
-        console.log('test');
-        break;*/
+     console.log('test');
+     break;*/
 }
--- 7-js入门/5.2-语句流程-循环.js
+++ 7-js入门/5.2-语句流程-循环.js
@@ -1,31 +1,34 @@
 /**
  * for循环 (WebStorm快捷命令itar)
  */
+for (var i = 0; i < obj.length; i++) {
+    var obj1 = obj[i];
 
+}
 
 /**
  * do-while (先干再说)
  * while (三思而后行)
  */
 
-var j=0;
-do{
+var j = 0;
+do {
     j++;
-    console.log('第'+j+'次');
+    console.log('第' + j + '次');
 }
-while (j<=3);
+while (j <= 3);
 
-var k=0;
-while (k<=3){
+var k = 0;
+while (k <= 3) {
     k++;
-    console.log('第'+k+'次');
+    console.log('第' + k + '次');
 }
 
 /**
  * for-in，循环遍历对象的属性 (WebStorm快捷命令itin)
  */
-var obj={
-    x:1,
-    y:'js',
-    z:true
+var obj = {
+    x: 1,
+    y: 'js',
+    z: true
 };
